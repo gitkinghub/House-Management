c302537cf9f389001f629ae8240b7bb5b60746f6
gitkinghub <mahiuhasamuel596@gmail.com>
2025-04-21 14:03:21 +0300

    resolved merge conflicts

--- taskful_api/task/permission.py
+++ taskful_api/task/permission.py
@@ -6,14 +6,20 @@ class IsAllowedToEditTaskListElseNone(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        
+
         if not request.user.is_anonymous:
             return True
-        
+
         return False
-    
+
     def has_object_permission(self, request, view, obj):
-        return request.user.profile == obj.created_by
+        # For editing operations, only the creator can edit
+        if request.method not in permissions.SAFE_METHODS:
+            return request.user.profile == obj.created_by
+
+        # For read operations, allow if user is a member of the house
+        # Check if the user's profile is in the house's members list
+        return request.user.profile in obj.house.members.all()
 
 class IsAllowedToEditTaskElseNone(permissions.BasePermission):
     """Permission to allow members of a house to access it"""
